c3e7cca6bab512f1327ed11b22de50b0c045bd50
jhyungit <jh021199@gmail.com>
2023-01-07 17:01:21 +0900

    greedy

--- greedy/3-2.py
+++ greedy/3-2.py
@@ -1,10 +1,15 @@
-print("plz type n and k")
-n, k = map(int , input().split())
+# 1이 될 때까지
+
+n, k = map(int , input("plz type n and k: ").split())
 
 cnt = 0
 
 while n != 1:
     if n % k == 0:
+        if k == 1:
+            n -= 1
+            cnt += 1
+            continue
         n //= k
         cnt += 1
     else:
